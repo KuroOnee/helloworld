191e6f431d1484dcd798f033e8c77ca09f2cc60a
KuroOnee <Dim22Rus@gmail.com>
2016-03-10 18:38:50 +0600

    edit mistake in last message

--- hello.c
+++ hello.c
@@ -5,6 +5,6 @@ int main()
     printf("Hello, world!\n");
     printf("Goodbye world.\n");
     printf("Just Goodbye. . .\n");
-    printf("Mmaybe See you soon!\n");
+    printf("Maybe See you soon!\n");
     return 0;
 }
