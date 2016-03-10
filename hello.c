5c3e7a8fe0c17b604dac2187a5e7039df11ad2bd
KuroOnee <Dim22Rus@gmail.com>
2016-03-10 18:41:58 +0600

    Edited message

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@ int main()
 {
     printf("Hello, world!\n");
     printf("Goodbye world.\n");
-    printf("Just Goodbye. . .\n");
-    printf("Mmaybe See you soon!\n");
+    printf("See you soon.\n");
+    printf("Maybe See you soon!\n");
     return 0;
 }
